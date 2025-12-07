825f18aee4a4e378c5afbbd9557be464d6914122
tobinskaiauliana <Ulianafencing@mail.ru>
2025-12-08 00:07:03 +0300

    .

--- database.py
+++ database.py
@@ -8,7 +8,6 @@ DB_PASSWORD = os.getenv("DB_PASSWORD", "auth_password")
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME", "itamhack")
-
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
